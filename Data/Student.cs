8f03ee7bec71e696e37efea2bcc11174a1cd0ee8
colmkill <colmand@live.com.au>
2024-03-29 14:50:21 +1030

    blah

--- Data/Student.cs
+++ Data/Student.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace SchoolmanagementAPP.mvc.Data;
 
 public partial class Student
 {
+   
     public int Id { get; set; }
 
     public string FirstName { get; set; } = null!;
